dba6025ca472c285a3098c082cecec74bd0e2840
Rafael Lossurdo <lossurdo@gmail.com>
2016-03-14 15:11:43 -0300

    conteúdo básico da aula 4

--- www/aula04/controller.js
+++ www/aula04/controller.js
@@ -2,8 +2,29 @@ angular.module('MeuApp', [])
 
 .controller('MeuController', ['$scope', function($scope) {
 
-	$scope.nome;
+	$scope.cpf = null;
+	$scope.email = '';
+	$scope.nome = '';
+	$scope.data_nascimento = '';
+	$scope.modalidade = '';
+	$scope.pace = '';
+
+	$scope.tempoMedioEstimado = '0';
 	
+	$scope.isVazio = function (obj) {
+		if(obj===null || obj==='') {
+			return 'invalido';
+		}
+	};
+
+	$scope.atualizarPace = function () {
+		$scope.tempoMedioEstimado = $scope.modalidade * $scope.pace;
+	};
+
+	$scope.inscrever = function () {
+		console.log('Inscrição efetuada com sucesso!');
+		alert('Inscrição efetuada com sucesso!');
+	};
 }])
 
 ;
